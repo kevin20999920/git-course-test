8d5f736148d4c7105855b9bc500475c212672908
fazt <contact@fazt.com>
2019-10-16 10:32:39 -0500

    mi primer comit

---
+++ app.js
@@ -0,0 +1,41 @@
+# 1.
+
+console.log('hellow word');
+
+# Antes de ingresar otro comando vamos a inicializar el repositorio de codigo
+# Para eso vamos a abrir un terminal. En este caso vamos a usar la consola de GIT 
+# Para eso vamos a la carpeta PROYECTO desde el escritorio
+# Le vamos a dar click derecho y seleccionamos la opcion GIT BASH HERE
+_________________________________________________________________________________________________
+# 2.
+
+# Ahora desde la consola GIT vamos a ingresar a la carpeta PROYECTO 
+
+
+OneDrive/Escritorio/proyecto
+
+_______________________________________________________________________________________________________
+# 3.
+
+# Ahora vamos a empezar a utilizar un comando de GIT para inicializar este proyecto
+
+
+# GIT INIT: Sirve para decirle al programa que vamos a empezar a utilizar GIT para controlar sus versiones
+
+
+# Vamos a digitar dentro de la consola GIT INIT. Va a crear una carpeta dentro de nuestro archivo PROYECTO
+# Esta nueva carpeta (.GIT) va a ser la encargada de administrar todos los cambios de nuestro codigo
+
+_________________________________________________________________________________________________________________
+# 4.
+
+# Ahora podemos usar un comando para ver en que archivos estamos trabajando en la consola GIT
+
+# GIT STATUS: Sirve para ver en que archivos estamos trabajando
+ # Cuando digitemos este metodo nos va a imprimir los archivos que hemos agregado para trabajar con ellos
+
+
+# Para agregar nuevos archivos vamos a usar GIT ADD 
+
+# GIT ADD: Sirve para agregar nuevos archivos 
+ # Se debe digitar este codigo y enseguida el archivo que queremos agregar 
